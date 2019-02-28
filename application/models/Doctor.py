ca1c588a2431f49ef01241ca82c659c2a50249da
DanielPri <privorotskyd@gmail.com>
2019-02-27 19:19:37 -0500

    #6 added doctor model

---
+++ application/models/Doctor.py
@@ -0,0 +1,52 @@
+from index import db
+from datetime import datetime
+from passlib.hash import sha256_crypt
+
+class Doctor(db.Model):
+	permit_number = db.Column(db.String(50), nullable=False)
+	fname = db.Column(db.String(30), nullable=False)
+	lname = db.Column(db.String(30), nullable=False)
+	specialty = db.Column(db.String(30), nullable=False)
+	password_hash = db.Column(db.String(100), nullable=False)
+	city = db.Column(db.String(30), nullable=False)
+
+	def __repr__(self):
+		return '<Doctor %r %r>' % self.fname % self.lname
+
+	
+
+
+# Initializes the database
+db.create_all()
+
+# Returns True if doctor exists
+def doctorExists(permit_number):
+	return Doctor.query.filter_by(permit_number=permit_number).first() is not None
+
+# Returns True if doctor is created
+def createDoctor(permit_number, fname, lname, specialty, password, city):
+	reponse = False
+	if doctorExists(permit_number):
+		reponse =  False # if doctor exists then return false
+	else:
+		# hash password
+		password_hash = sha256_crypt.hash(password)
+
+		# Create the new doctor
+		newDoctor = Doctor(permit_number=permit_number, fname=fname, lname=lname, password_hash=password_hash, specialty=specialty, city=city)
+
+		# Add it to the database
+		db.session.add(newDoctor)
+
+		# Commit it
+		db.session.commit()
+
+		reponse = True
+	return reponse
+
+
+
+
+
+
+
--- application/models/Nurse.py
+++ application/models/Nurse.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from passlib.hash import sha256_crypt
 
 class Nurse(db.Model):
+	access_ID = db.Column(db.String(100), nullable=False)
 	fname = db.Column(db.String(30), nullable=False)
 	lname = db.Column(db.String(30), nullable=False)
 	password_hash = db.Column(db.String(100), nullable=False)
-	access_ID = db.Column(db.String(100), nullable=False)
 
 	def __repr__(self):
 		return '<Nurse %r %r>' % self.fname % self.lname
@@ -22,9 +22,9 @@ def nurseExists(access_ID):
 	return Nurse.query.filter_by(access_ID=access_ID).first() is not None
 
 # Returns True if nurse is created
-def createNurse(fname, lname, password, access_ID):
+def createNurse(access_ID, fname, lname, password):
 	reponse = False
-	if nurseExists(hcnumber):
+	if nurseExists(access_ID):
 		reponse =  False # if nurse exists then return false
 	else:
 		# hash password
