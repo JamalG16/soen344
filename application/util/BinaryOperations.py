ddae6585c0733fbab5a2169cfb6e9af05b4657c9
aiceball <abikalapov+git@gmail.com>
2019-03-05 21:19:16 -0500

    binary madness

--- application/util/BinaryOperations.py
+++ application/util/BinaryOperations.py
@@ -1,13 +1,28 @@
 def main():
     print("hello world")
-    binaryString = convertBooleanListToBinaryString([True,False,True])
-    print(binaryString)
-    booleanArray = convertBinaryStringToBooleanList(binaryString)
-    print(booleanArray)
+    binaryString1 = convertBooleanListToBinaryString([False,False, True,False,True])
+    print(binaryString1)
+    booleanArray1 = convertBinaryStringToBooleanList(binaryString1)
+    print(booleanArray1)
+
+    binaryString2 = convertBooleanListToBinaryString([False, True, True, False, True])
+    print(binaryString2)
+    booleanArray2 = convertBinaryStringToBooleanList(binaryString2)
+    print(booleanArray2)
+
+    ten = '01010'
+    twelve = '01100'
+    print(andTwoBinaryStrings(binaryString1,binaryString2))
+    print(convertIntegerToBooleanList(andTwoBinaryStrings(binaryString1, binaryString2)))
 
 
 def convertBooleanListToBinaryString(booleanList):
     return ''.join(['1' if x else '0' for x in booleanList])
+
+def convertIntegerToBooleanList(integer):
+    print(str(integer))
+    stringed = format(integer,"036b")
+    return convertBinaryStringToBooleanList(stringed)
 
 def convertBinaryStringToBooleanList(binaryString):
     booleanList=[]
@@ -16,6 +31,8 @@ def convertBinaryStringToBooleanList(binaryString):
         else: booleanList.append(False)
     return booleanList
 
+def andTwoBinaryStrings(string1, string2):
+    return int(string1,2) & int(string2,2)
 
 if __name__ == "__main__":
     main()
