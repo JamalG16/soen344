72a0e830b3c4fcbf061021f470c37b0f1bd975bb
Jessica Allaire <jessica.allaire.96@hotmail.com>
2019-03-06 11:57:23 -0500

    #42 Added Appointment Table
    Added basic layout
    co-authored-by: Mila Roisin mila.roisin@gmail.com

--- static/src/components/body/CalendarPatient.js
+++ static/src/components/body/CalendarPatient.js
@@ -1,10 +1,15 @@
 import {Component} from "react";
 import React from "react";
-import { Calendar, Alert , Select} from 'antd';
+import { Calendar, Alert , Table, Button, Icon} from 'antd';
 import * as moment from 'moment';
 import 'antd/es/calendar/style/index.css';
 import 'antd/es/alert/style/index.css';
+import 'antd/es/table/style/index.css';
+import 'antd/es/divider/style/index.css';
+import 'antd/es/button/style/index.css';
 import 'antd/es/select/style/index.css';
+import 'antd/es/pagination/style/index.css';
+import 'antd/es/icon/style/index.css';
 
 class CalendarPatient extends Component {
 
@@ -27,23 +32,52 @@ class CalendarPatient extends Component {
 
     render() {
         const { current, value, selectedValue } = this.state;
-        let message;
+        let message, success;
 
         if (selectedValue < current) {
             message = "You cannot select a previous date to book an appointment";
+            success = false;
         } else
         {
             message = "You selected date: " + selectedValue.format('YYYY-MM-DD');
+            success = true;
         }
 
         return (
             <div>
                 <Alert message={message}/>
                 <Calendar value={value} onSelect={this.onSelect} onPanelChange={this.onPanelChange}/>
+                <AppointmentTable success={success} value={selectedValue}/>
             </div>
         );
     }
 
+}
+
+function AppointmentTable(props) {
+
+    const columns = [{
+      title: 'Time',
+      dataIndex: 'time',
+    }, {
+      dataIndex: "button"
+    }];
+
+    const data = [{
+      time: moment().format('HH:mm A') + " - " + (moment().add(20, 'minutes')).format('HH:mm A'),
+      button: <Button type="primary" icon="plus" size="large">ADD TO CART</Button>
+    }];
+
+
+    if (!props.success) {
+        return null;
+    }
+    return (
+        <div>
+            <h2>Available Appointments for {props.value.format('YYYY-MM-DD')}</h2>
+            <Table columns={columns} dataSource={data} pagination={false}/>
+        </div>
+    );
 }
 
 export default CalendarPatient;
