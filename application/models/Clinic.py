3854c2b00b2f2c56eadabac4f1bf14767ef3b93c
Mila Roisin <mila.roisin@gmail.com>
2019-04-04 19:41:51 -0400

    [#74] Updated models for second clinic in Appointment, Clinic and DoctorSchedule
    Co-authored-by: Anamika Pancholy @MickeyPa <anamika.pancholy@gmail.com>

--- application/models/Clinic.py
+++ application/models/Clinic.py
@@ -2,11 +2,13 @@ from index import db
 
 class Clinic(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    clinic_id = db.Column(db.Integer(), nullable=False, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     address = db.Column(db.String(), nullable=False, unique=True)
 
     def __iter__(self):
         yield 'id', self.id
+        yield 'clinic_id', self.clinic_id
         yield 'name', self.name
         yield 'address', self.address
 
--- application/models/DoctorSchedule.py
+++ application/models/DoctorSchedule.py
@@ -3,15 +3,15 @@ from index import db
 
 class DoctorSchedule(db.Model):
     permit_number = db.Column(db.String(), nullable=False, primary_key=True)
+    clinic_id = db.Column(db.Integer, db.ForeignKey('clinic.id'), nullable=False, primary_key=True)
     date = db.Column(db.String(), nullable=False, primary_key=True)
     timeSlots = db.Column(db.String(), nullable=False)
-    clinic_id = db.Column(db.Integer, db.ForeignKey('clinic.id'), nullable=False, primary_key=True)
 
     def __iter__(self):
         yield 'permit_number', self.permit_number
+        yield 'clinic_id', self.clinic_id
         yield 'date', self.date
         yield 'timeSlots', self.timeSlots
-        yield 'clinic_id', self.clinic_id
 
 
 # Initializes the database
